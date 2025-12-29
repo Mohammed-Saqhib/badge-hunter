f3cd358afb26a69004d652330b3abfccb7d7d588
Mohammed Saqhib <msaqhib76@gmail.com>
2025-12-29 21:20:35 +0530

    Speed run for Quickdraw

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def main():
     print("Hello, GitHub Badges!")
     print("I am ready to earn the Pull Shark badge!")
+    print("Gotta go fast for Quickdraw!")
 
 if __name__ == "__main__":
     main()
